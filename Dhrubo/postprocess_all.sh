245a10daa911766282122698b305eeef20f17d86
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-06-11 00:56:11 -0400

    Inverting print order Hidden size first

--- Dhrubo/postprocess_all.sh
+++ Dhrubo/postprocess_all.sh
@@ -12,12 +12,15 @@ list_files=(
 
 list_hiddensize=(16 32 64)
 
+# Collate outputs first
 for l in ${list_files[@]}; do
-    echo -e "\n\n\t\t-------------------- Processing $l ---------------------"
     sh ../hpc_scripts/4_collate_output_splits.sh $l > $l.out
+done
 
-    for h in ${list_hiddensize[@]}; do
-        echo -e "\n\t----- Hidden size = $h -----"
+for h in ${list_hiddensize[@]}; do
+    echo -e "\n\n\t\t-------------------- Hidden size = $h ---------------------"
+    for l in ${list_files[@]}; do
+        echo -e "\n\t----- Processing $l -----"
         python3 ../hpc_scripts/5_compute_best_2tierEMI_hiddenfiltered.py $l.out $h
     done
 done
