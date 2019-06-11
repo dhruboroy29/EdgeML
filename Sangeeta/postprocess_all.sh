2e63ec17721225f24c4521c9ac4ed9289643517b
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-06-11 00:46:25 -0400

    Adding postprocessing script for single 3class EMI

---
+++ Sangeeta/postprocess_all.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+source activate tfgpu
+
+list_files=(
+            singleEMI_3class_winlen_256
+            singleEMI_3class_winlen_384
+            singleEMI_3class_winlen_512
+            singleEMI_3class_winlen_640
+            singleEMI_3class_winlen_768
+            )
+
+list_hiddensize=(16 32 64)
+
+for l in ${list_files[@]}; do
+    echo -e "\n\n\t\t-------------------- Processing $l ---------------------"
+    sh ../hpc_scripts/4_collate_output_splits.sh $l > $l.out
+
+    for h in ${list_hiddensize[@]}; do
+        echo -e "\n\t----- Hidden size = $h -----"
+        python3 ../hpc_scripts/5_compute_best_EMI_hiddenfiltered.py $l.out $h
+    done
+done
