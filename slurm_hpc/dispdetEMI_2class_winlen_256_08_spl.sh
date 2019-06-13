c6e4ee66bb7ee15705074e141380f1a9c32851c7
ksangeeta2429 <kumari.14@osu.edu>
2019-06-13 18:16:31 -0400

    adding split scripts

---
+++ slurm_hpc/dispdetEMI_2class_winlen_256_08_spl.sh
@@ -0,0 +1,2 @@
+outname=`echo $0 | sed "s/.sh/.out/g"`
+python3 ../tf/examples/EMI-RNN/step2_emi_fastrgnn_disp_det.py -O 2 -gN sigmoid -uN sigmoid -bs 64 -H 32 -Dat /scratch/dr2915/Bumblebee/bb_3class_winlen_256_winindex_all/48_16 -rnd 10 -it 5 -ep 10 -k 100 -out $outname
