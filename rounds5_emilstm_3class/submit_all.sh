fb22e006815069f02a93674757ef5cbb52a0cf66
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-06-20 14:31:19 -0400

    Submit all scripts

---
+++ rounds5_emilstm_3class/submit_all.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+list_files=(
+            3_SUBMIT_rounds5_lstmEMI_3class_winlen_256_jobs.sh
+            3_SUBMIT_rounds5_lstmEMI_3class_winlen_384_jobs.sh
+            3_SUBMIT_rounds5_lstmEMI_3class_winlen_512_jobs.sh
+            3_SUBMIT_rounds5_lstmEMI_3class_winlen_640_jobs.sh
+            3_SUBMIT_rounds5_lstmEMI_3class_winlen_768_jobs.sh
+            )
+
+cd ../slurm_hpc
+
+# Submit jobs
+for l in ${list_files[@]}; do
+    sh $l
+    sleep 60
+done
