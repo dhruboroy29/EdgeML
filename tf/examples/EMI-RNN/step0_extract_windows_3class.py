840107a679091609ff11d7a70e285fde6cb17e3d
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-10-13 16:25:52 -0400

    Modifying window extraction script for BuildSys demo files

--- tf/examples/EMI-RNN/step0_extract_windows_3class.py
+++ tf/examples/EMI-RNN/step0_extract_windows_3class.py
@@ -20,7 +20,6 @@ def extract_windows(indirs, outdir, class_label, stride, winlen, samprate=256, m
 
     assert isinstance(indirs, (str, list))
     assert isinstance(outdir, str)
-    assert stride == winlen
 
     # If single directory given, create list
     if isinstance(indirs, str):
@@ -47,6 +46,7 @@ def extract_windows(indirs, outdir, class_label, stride, winlen, samprate=256, m
 
         # Find data files
         list_files = glob.glob(os.path.join(indir,'*.data'))
+        list_files.extend(glob.glob(os.path.join(indir, '*.bbs')))
 
         for cur_file in list_files:
             # Get filename without extension
@@ -109,86 +109,27 @@ def extract_windows(indirs, outdir, class_label, stride, winlen, samprate=256, m
 
 # Test
 if __name__=='__main__':
-    print('----------------Human BumbleBee Targets----------------')
-    # Bumblebee human cuts
-    extract_windows(indirs=[
-        '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/arc_1 (Humans_Gym balls)/Human/',
-    '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/bv_4 (Humans_Cars)/Human/',
-    '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/ceiling_238_10 (Humans_Gym balls)/Human/',
-    '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/combined_5 (Humans_Dogs)/11-30-2011/Human',
-    '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/combined_5 (Humans_Dogs)/Human/',
-    '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/kh_3 (Humans_Gym balls)/Human/',
-    '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/prb_2 (Humans_Gym balls)/Human/',
-    '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/Parking garage orthogonal (Humans)/',
-    '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/Parking garage radial (Humans)/'
-    ],
+    demo_base_folder = '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Austere/BuildSys_Demo/'
 
-        outdir='/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/'
-               'Deep_Learning_Radar/Displacement_Detection/Data/BumbleBee/',
-        class_label='Human',
-        stride=384,
-        winlen=384)
-
-    print('----------------Non-human BumbleBee Targets----------------')
-    # Bumblebee dog cuts
-    extract_windows(indirs=[
-        '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/arc_1 (Humans_Gym balls)/Ball/',
-        '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/bv_4 (Humans_Cars)/Car/',
-        '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/ceiling_238_10 (Humans_Gym balls)/Ball/',
-        '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/combined_5 (Humans_Dogs)/Dog/',
-        '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/kh_3 (Humans_Gym balls)/Dog/',
-        '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/prb_2 (Humans_Gym balls)/Dog/',
-        '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/osu_farm_meadow_may24-28_2016 (Cattle)/',
-        '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/Radar_site1_hilltop (Cattle)/',
-        '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/Targets/Radar_site2_creamery (Cattle)/'
-    ],
-
-        outdir='/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/'
-               'Deep_Learning_Radar/Displacement_Detection/Data/BumbleBee/',
-        class_label='Nonhuman',
-        stride=384,
-        winlen=384)
-
-    print('----------------BumbleBee Noise----------------')
-    # Bumblebee noise
-    bb_noise_base = '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Bumblebee/'
-    extract_windows(indirs=bb_noise_base + 'Noise',
-                    outdir='/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/'
-                           'Deep_Learning_Radar/Displacement_Detection/Data/BumbleBee/',
-                    class_label='Noise',
-                    stride=384,
-                    winlen=384)
-    exit()
+    print('Extracting Windows for BuildSys''19 demo training')
 
     print('----------------Austere Humans----------------')
-    # New cuts
-    austere_base_folder = '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/' \
-                          'Austere/Bora_New_Detector/' \
-                          'Bora_new_det_aust_th_22_bgr_19_M_30_N_128_window_res_lookahead_last_wind_padded/'
-    extract_windows(indirs=austere_base_folder + 'austere_384_human',
-                    outdir='/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/'
-                           'Deep_Learning_Radar/Displacement_Detection/Data/Austere/Bora_New_Detector/',
-                    class_label='Humans',
-                    stride=384,
-                    winlen=384)
+    extract_windows(indirs=demo_base_folder + 'Raw/final_human_full_cuts',
+                    outdir=demo_base_folder + 'Windowed/',
+                    class_label='Human',
+                    stride=128,
+                    winlen=256)
 
-    print('----------------Austere Non-humans----------------')
-    # New cuts
-    austere_base_folder = '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/' \
-                          'Austere/Bora_New_Detector/' \
-                          'Bora_new_det_aust_th_22_bgr_19_M_30_N_128_window_res_lookahead_last_wind_padded/'
-    extract_windows(indirs=austere_base_folder + 'austere_304_cow',
-        outdir='/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/'
-               'Deep_Learning_Radar/Displacement_Detection/Data/Austere/Bora_New_Detector/',
-        class_label='Nonhumans',
-        stride=384,
-        winlen=384)
+    print('----------------Austere Bikes----------------')
+    extract_windows(indirs=demo_base_folder + 'Raw/final_bike_radial_full_cuts',
+                    outdir=demo_base_folder + 'Windowed/',
+                    class_label='Bike',
+                    stride=128,
+                    winlen=256)
 
-    print('----------------Austere Noise----------------')
-    austere_noise_base = '/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/Deep_Learning_Radar/Data/Austere/'
-    extract_windows(indirs=austere_noise_base + 'Noise',
-                    outdir='/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/Research/'
-                           'Deep_Learning_Radar/Displacement_Detection/Data/Austere/Bora_New_Detector/',
-                    class_label='Noise',
-                    stride=384,
-                    winlen=384)
+    print('----------------Austere Clutter----------------')
+    extract_windows(indirs=demo_base_folder + 'Raw/clutter',
+                    outdir=demo_base_folder + 'Windowed/',
+                    class_label='Clutter',
+                    stride=128,
+                    winlen=256)
