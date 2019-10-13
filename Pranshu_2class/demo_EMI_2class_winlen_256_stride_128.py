a5ead716793ecc7e2282eecef2ab1de82e82e1f3
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-10-13 19:05:15 -0400

    Adding 2-class scripts for Pranshu

---
+++ Pranshu_2class/demo_EMI_2class_winlen_256_stride_128.py
@@ -0,0 +1,60 @@
+import sys
+import os
+
+sys.path.append('../')
+
+######################### ONLY MODIFY THESE VALUES #########################
+# Number of splits of hyperparam file
+winlen = 256
+stride = 128
+num_splits='12'
+
+# Base path of data
+prefix = 'demo_EMI_2class_winlen_' + str(winlen) + '_stride_' + str(stride)
+
+base='/mnt/6b93b438-a3d4-40d2-9f3d-d8cdbb850183/' \
+     'Research/Deep_Learning_Radar/Data/Austere/BuildSys_Demo/' \
+     'Windowed/winlen_' + str(winlen) + '_stride_' + str(stride) + '/12_8'
+
+# Batch system
+bat_sys='pbs'
+
+# List of values of hyperparam k
+k=[5, 6, 7]
+
+# Running time
+walltime='1-0'
+
+######################### KEEP THE REST INTACT #########################
+# Folder where jobs are saved
+jobfolder = '../'+ bat_sys +'_hpc/'
+
+#Init args
+init_argv=sys.argv
+
+# Enter hpc_scripts folder
+os.chdir('../hpc_scripts')
+
+# Prepare data
+#print('###### Scripts/processing_data #####')
+#sys.argv=init_argv+['-type', prefix, '-base', base]
+#import Scripts.create_train_val_test_split
+
+# Generate gridsearch
+print('###### hpc_scripts/gridsearch #####')
+sys.argv=init_argv+['-bat', bat_sys, '-type', prefix, '-base',
+                    base, '-O', str(2), '-ots', str(winlen) ,'-k']+[str(param) for param in k]
+import hpc_scripts.gridsearch_0_EMI
+
+# Split hyperparam file
+print('###### hpc_scripts/split_hyp_wrapper #####')
+sys.argv=init_argv+[jobfolder+prefix+'.sh',num_splits]
+import hpc_scripts.split_hyp_wrapper_1
+
+# Create batch job
+print('###### hpc_scripts/create_batch_wrapper #####')
+sys.argv=init_argv+[jobfolder+prefix+'_',walltime,bat_sys]
+import hpc_scripts.create_batch_wrapper_2
+
+# Submit
+print("\nNow submit " + bat_sys + "_hpc/3_SUBMIT_"+prefix+"_jobs.sh on server")
--- hpc_scripts/gridsearch_0_EMI.py
+++ hpc_scripts/gridsearch_0_EMI.py
@@ -15,9 +15,8 @@ parser.add_argument('-ots', type=int, help='Original number of time steps')
 parser.add_argument('-rnd', type=int, default=5, help='Number of rounds')
 parser.add_argument('-ep', type=int, default=50, help='Number of epochs')
 parser.add_argument('-it', type=int, default=10, help='Number of iterations')
-parser.add_argument('-k', type=int, default=10, help='Min. number of consecutive target instances. 100 for max possible')
+parser.add_argument('-k', nargs='+', help='List of hyperparams for k', required=True)
 parser.add_argument('-q15', type=ast.literal_eval, default=False, help='Is this a Q15 gridsearch?')
-
 
 if len(sys.argv) < 2:
     parser.print_help()
@@ -25,33 +24,27 @@ if len(sys.argv) < 2:
 
 args = parser.parse_args()
 
-if args.q15:
-    p0 = ['quantSigm', 'quantTanh']  # ['relu','quantSigm','quantTanh'] #['quantSigm']
-    p1 = ['quantTanh', 'quantSigm']  # ['relu','quantSigm','quantTanh'] #['quantTanh']
-else:
-    p0 = ['sigmoid', 'tanh']
-    p1 = ['tanh', 'sigmoid']
+p0 = ['quantSigm', 'quantTanh']  # ['relu','quantSigm','quantTanh'] #['quantSigm']
+p1 = ['quantTanh', 'quantSigm']  # ['relu','quantSigm','quantTanh'] #['quantTanh']
 
 p2 = [64, 128]
 p3 = [16, 32, 64]
+p4 = args.k
 
 out_folder = os.path.join('..', args.bat + '_hpc')
-out_suffix = ''
-if args.q15:
-    out_suffix += '_q15'
-out_file = os.path.join('..', args.bat + '_hpc', args.type + out_suffix + '.sh')
+out_file = os.path.join('..', args.bat + '_hpc', args.type + '.sh')
 
 
 def generate_trainstring(v):
     res_str = "python3 ../tf/examples/EMI-RNN/step2_emi_fastrgnn_disp_det.py -O " + str(args.O)\
               + " -gN " + str(v[0]) + " -uN " + str(v[1]) + " -bs " + str(v[2]) + " -H " + str(
         v[3]) + " -Dat " + args.base\
-              + " -rnd " + str(args.rnd) + " -it " + str(args.it) + " -ep " + str(args.ep) + " -ots " + str(args.ots) + " -k " + str(args.k) + " -out $outname"
+              + " -rnd " + str(args.rnd) + " -it " + str(args.it) + " -ep " + str(args.ep) + " -ots " + str(args.ots) + " -k " + str(v[4]) + " -out $outname"
 
     return res_str
 
 pool = ThreadPool()
-hyperparams = np.asarray(list(itertools.product(p0, p1, p2, p3))).tolist()
+hyperparams = np.asarray(list(itertools.product(p0, p1, p2, p3, p4))).tolist()
 results = pool.map(generate_trainstring, hyperparams)
 
 # Flatten
