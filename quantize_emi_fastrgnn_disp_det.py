ec82fd6917ab39cced092b10cdb481e7fd340dc0
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-10-22 00:53:41 -0400

    Marking where Pranshu needs to enter his code

--- quantize_emi_fastrgnn_disp_det.py
+++ quantize_emi_fastrgnn_disp_det.py
@@ -70,121 +70,16 @@ x_test, y_test = np.load(os.path.join(data_dir,'x_test.npy')), np.load(os.path.j
 BAG_TEST = np.argmax(y_test[:, 0, :], axis=1)
 
 # Inferred params
-NUM_SUBINSTANCE = x_train.shape[1]
-NUM_TIMESTEPS = x_train.shape[2]
-NUM_FEATS = x_train.shape[-1]
+NUM_SUBINSTANCE = x_test.shape[1]
+NUM_TIMESTEPS = x_test.shape[2]
+NUM_FEATS = x_test.shape[-1]
 
-print("x_train shape is:", x_train.shape)
-print("y_train shape is:", y_train.shape)
-print("x_test shape is:", x_val.shape)
-print("y_test shape is:", y_val.shape)
-
-# Adjustment for max k: number of subinstances
-if k==100:
-    k = x_train.shape[1]
-
-
-# Define the linear secondary classifier
-def createExtendedGraph(self, baseOutput, *args, **kwargs):
-    W1 = tf.Variable(np.random.normal(size=[NUM_HIDDEN, NUM_OUTPUT]).astype('float32'), name='W1')
-    B1 = tf.Variable(np.random.normal(size=[NUM_OUTPUT]).astype('float32'), name='B1')
-    y_cap = tf.add(tf.tensordot(baseOutput, W1, axes=1), B1, name='y_cap_tata')
-    self.output = y_cap
-    self.graphCreated = True
-
-
-def restoreExtendedGraph(self, graph, *args, **kwargs):
-    y_cap = graph.get_tensor_by_name('y_cap_tata:0')
-    self.output = y_cap
-    self.graphCreated = True
-
-
-def feedDictFunc(self, keep_prob=None, inference=False, **kwargs):
-    if inference is False:
-        feedDict = {self._emiGraph.keep_prob: keep_prob}
-    else:
-        feedDict = {self._emiGraph.keep_prob: 1.0}
-    return feedDict
-
-
-EMI_FastGRNN._createExtendedGraph = createExtendedGraph
-EMI_FastGRNN._restoreExtendedGraph = restoreExtendedGraph
-if USE_DROPOUT is True:
-    EMI_FastGRNN.feedDictFunc = feedDictFunc
-
-inputPipeline = EMI_DataPipeline(NUM_SUBINSTANCE, NUM_TIMESTEPS, NUM_FEATS, NUM_OUTPUT)
-emiFastGRNN = EMI_FastGRNN(NUM_SUBINSTANCE, NUM_HIDDEN, NUM_TIMESTEPS, NUM_FEATS, wRank=WRANK, uRank=URANK,
-                           gate_non_linearity=GATE_NL, update_non_linearity=UPDATE_NL, useDropout=USE_DROPOUT)
-emiTrainer = EMI_Trainer(NUM_TIMESTEPS, NUM_OUTPUT, lossType='xentropy')
-
-
-# Connect elementary parts together to create forward graph
-tf.reset_default_graph()
-g1 = tf.Graph()
-with g1.as_default():
-    # Obtain the iterators to each batch of the data
-    x_batch, y_batch = inputPipeline()
-    # Create the forward computation graph based on the iterators
-    y_cap = emiFastGRNN(x_batch)
-    # Create loss graphs and training routines
-    emiTrainer(y_cap, y_batch)
-
-
-with g1.as_default():
-    emiDriver = EMI_Driver(inputPipeline, emiFastGRNN, emiTrainer)
-
-emiDriver.initializeSession(g1, config=config)
+print("x_test shape is:", x_test.shape)
+print("y_test shape is:", y_test.shape)
 
 '''
-Evaluating the  trained model
+Insert Pranshu logic for instance-level predictions
 '''
-
-# Early Prediction Policy: We make an early prediction based on the predicted classes
-#     probability. If the predicted class probability > minProb at some step, we make
-#     a prediction at that step.
-def earlyPolicy_minProb(instanceOut, minProb, **kwargs):
-    assert instanceOut.ndim == 2
-    classes = np.argmax(instanceOut, axis=1)
-    prob = np.max(instanceOut, axis=1)
-    index = np.where(prob >= minProb)[0]
-    if len(index) == 0:
-        assert (len(instanceOut) - 1) == (len(classes) - 1)
-        return classes[-1], len(instanceOut) - 1
-    index = index[0]
-    return classes[index], index
-
-def getEarlySaving(predictionStep, numTimeSteps, returnTotal=False):
-    predictionStep = predictionStep + 1
-    predictionStep = np.reshape(predictionStep, -1)
-    totalSteps = np.sum(predictionStep)
-    maxSteps = len(predictionStep) * numTimeSteps
-    savings = 1.0 - (totalSteps / maxSteps)
-    if returnTotal:
-        return savings, totalSteps
-    return savings
-
-# Pick the best model
-devnull = open(os.devnull, 'r')
-acc = 0.0
-
-# Read model stats file
-with open(os.path.join(data_dir, 'modelstats_O=' + str(NUM_OUTPUT) + '_H=' + str(NUM_HIDDEN) + '_k=' + str(k)
-                                          + '_gN=' + GATE_NL + '_uN=' + UPDATE_NL + '_ep=' + str(NUM_EPOCHS)
-                                          + '_it=' + str(NUM_ITER) + '_rnd=' + str(NUM_ROUNDS)
-                                          + '_bs=' + str(BATCH_SIZE) + '.csv'), 'r') as stats_csv:
-    modelStats = csv.reader(stats_csv)
-    header = next(modelStats)
-    for row in modelStats:
-        c_round_, c_acc, c_modelPrefix, c_globalStep = row
-        if float(c_acc) > acc:
-            round_, acc, modelPrefix, globalStep = int(c_round_), float(c_acc), c_modelPrefix, int(c_globalStep)
-
-print('Best Model: ', modelPrefix, globalStep)
-
-graph = emiDriver.loadSavedGraphToNewSession(modelPrefix, globalStep, redirFile=devnull)
-
-predictions, predictionStep = emiDriver.getInstancePredictions(x_test, y_test, earlyPolicy_minProb,
-                                                               minProb=0.99, keep_prob=1.0)
 
 bagPredictions = emiDriver.getBagPredictions(predictions, k=k, numClass=NUM_OUTPUT)
 print("Round: %2d, window length: %3d, Validation accuracy: %.4f" % (round_, ORIGINAL_NUM_TIMESTEPS, acc), end='')
