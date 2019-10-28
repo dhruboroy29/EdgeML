7307095b24992ce48514c572f83cdb6cea84b12d
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-10-28 13:19:43 -0400

    Minor changes

--- 03_run_unit_test.sh
+++ 03_run_unit_test.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+# Run Python pipeline
+echo "Running Python inference pipeline on test data..."
+python3 "C++/emi_fastgrnn.py"
+
+# Run C++ pipeline
+echo "Done! Running C++ inference pipeline on test data..."
+cd "C++"
+g++ "emi_fastgrnn.cpp"
+./a.out
+
+# Show diffs of outputs
+echo "Test complete. Printing diff of outputs..."
+diff out_py.csv out_c++.csv
