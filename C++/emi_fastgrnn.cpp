404eff646bc890ecd9380e66f29ee6d8bd1e2e03
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-10-29 00:24:50 -0400

    Adding preproc directives MOTE, DBG, TIME

--- C++/emi_fastgrnn.cpp
+++ C++/emi_fastgrnn.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <sstream>
 #include <algorithm>
+#ifndef MOTE
+#include <fstream>
+#endif
+#ifdef TIME
+#include <ctime>
+#endif
 
 #include "model_params.h"
 #include "test_data.h"
 
 using namespace std;
+
+#ifdef TIME
+	#undef DBG
+#endif
 
 #define min(a,b) ((b)>(a))?a:b
 #define max(a,b) ((a)>(b))?a:b
@@ -86,17 +95,17 @@ void quantSigm(ll* vec, int vec_len, ll scale, ll* out_vec){
 // Print utils
 void util_printVec(ll* vec, int vec_len){
 	for(int i=0; i < vec_len; i++)
-		printf("%lli\t", vec[i]);
-	printf("\n\n");
+		cout << vec[i] << '\t';
+	cout << "\n\n";
 }
 
 void util_printMatrix(uint* mat, int row_len, int col_len){
 	for(int i=0; i < row_len; i++){
 		for(int j=0; j < col_len; j++)
-			printf("%u\t", *(mat+i*col_len+j));
-		printf("\n");
+			cout << *(mat+i*col_len+j) << '\t';
+		cout << endl;
 	}
-	printf("\n\n");
+	cout << "\n\n";
 }
 
 // Matrix slice utils
@@ -124,26 +133,30 @@ string strBuild(ll i, char delim)
 
 int main(){
 	int size = sizeof(qW1_transp_l) + sizeof(qFC_Bias_l) + sizeof(qW2_transp_l) + sizeof(qU2_transp_l) + sizeof(qFC_Weight_l) + sizeof(qU1_transp_l) + sizeof(qB_g_l) + sizeof(qB_h_l) + sizeof(q_l) + sizeof(I_l) + sizeof(mean_l) + sizeof(stdev_l) + sizeof(I_l_vec) + sizeof(q_times_I_l);
-	printf("Model size: %d KB\n\n", size/1000);
-
+	
+#ifdef DBG
+	cout << "Model size: " << size/1000 << " KB" << endl << endl;
+#endif
+#ifndef MOTE
 	// Initialize output file
 	ofstream outfile;
 	outfile.open("out_c++.csv");
-
-	//uint test_input[][8] = {{2032,2041,2019,2014,2034,2021,2038,2053},{2017,2051,2017,2041,2039,2050,2013,2056},{2016,2035,2021,2040,2008,2059,2026,2046},{2049,2050,2026,2067,2025,2041,2039,2025},{2025,2056,1994,2039,2015,2037,2029,2049},{2018,2060,2014,2046,2036,2029,2018,2045},{2020,2031,2037,2023,2001,2056,2002,2029},{2031,2028,2041,2059,2024,2060,2052,2036},{2045,2034,2012,2030,2019,1965,2039,2023},{2014,2073,2015,2031,2054,2026,2028,2060},{2023,2020,2046,2020,2035,2037,2012,2029},{2050,2019,2055,2042,2019,2063,2020,2043}};
-
-	//uint test_input2[][8] = {{2045,2034,2012,2030,2019,1965,2039,2023},{2014,2073,2015,2031,2054,2026,2028,2060},{2023,2020,2046,2020,2035,2037,2012,2029},{2050,2019,2055,2042,2019,2063,2020,2043},{2039,2043,2013,2059,2001,2020,2044,2034},{2029,2058,2002,2046,2040,2017,2051,2050},{2026,2070,2034,2020,2042,2019,2009,2042},{2019,2031,2054,2046,2040,2058,2033,2048},{2045,2027,2037,2070,2009,2076,2026,2026},{2051,2047,2005,2055,2009,2030,2043,2057},{2017,2059,2020,2049,2041,2052,2007,2049},{2021,2016,2028,2036,2015,2052,2007,2043}};
-
+#endif
 	for(int d=0; d < numData; d ++)
 	{
 		uint test_input[timeSteps][inputDims] = {0};
 		util_slice3D((uint*) test_inputs, (uint*) test_input, d, timeSteps, inputDims);
-		//util_printMatrix((uint*) test_input, timeSteps, inputDims);
+
+#ifdef DBG
+		util_printMatrix((uint*) test_input, timeSteps, inputDims);
+#endif
 
 		ll h[hiddenDims] = {0};
 	
 		for(int t=0; t<timeSteps; t++){
-			//util_printVec(h, hiddenDims);
+#ifdef TIME			
+			clock_t startTime = clock(); //Start timer
+#endif
 			uint x_int[inputDims] = {0};
 			util_slice2D((uint*)test_input, x_int, t, inputDims);
 	
@@ -151,12 +164,12 @@ int main(){
 	
 			copyUIntVecToLL(x_int, x, inputDims);
 	
-			//printf("Current input array in ll\n");
+			//cout << "Current input array in ll" << endl;
 			//util_printVec(x, inputDims);
 	
 			stdScaleInput(x, inputDims, x);
 	
-			//printf("Post-standardization input\n");
+			//cout << "Post-standardization input" << endl;
 			//util_printVec(x, inputDims);
 	
 			// Precompute
@@ -171,12 +184,12 @@ int main(){
 	
 			addVecs(pre, out_hiddenDims, hiddenDims, pre);
 	
-			//util_printVec(pre, hiddenDims);
-	
 			divVecScal(pre, q_l, hiddenDims, pre);
-	
-			//printf("Pre at t=%d:\n", t);
-			//util_printVec(pre, hiddenDims);
+
+#ifdef DBG
+			cout << "Pre at t=" << t << endl;
+			util_printVec(pre, hiddenDims);
+#endif
 	
 			// Create h_, z
 			ll h_[hiddenDims] = {0};
@@ -190,13 +203,13 @@ int main(){
 	
 			GATE_NL(z, hiddenDims, q_times_I_l, z);
 			divVecScal(z, q_l, hiddenDims, z);
+#ifdef DBG
+			cout << "h_ at t=" << t << endl;
+			util_printVec(h_, hiddenDims);
 	
-			//printf("h_ at t=%d:\n", t);
-			//util_printVec(h_, hiddenDims);
-	
-			//printf("z at t=%d:\n", t);
-			//util_printVec(z, hiddenDims);
-	
+			cout << "z at t=" << t << endl;
+			util_printVec(z, hiddenDims);
+#endif
 			// Create new h
 			mulVecs(z, h, hiddenDims, h);
 	
@@ -205,25 +218,33 @@ int main(){
 	
 			addVecs(h, out_hiddenDims, hiddenDims, h);
 			divVecScal(h, I_l, hiddenDims, h);
-	
-			//printf("h at t=%d:\n", t);
-			//util_printVec(h, hiddenDims);
+#ifdef TIME
+			double msPassed = (clock() - startTime) * 1e3 / (double) CLOCKS_PER_SEC;
+			cout << "Loop running time: " << msPassed << " ms" << endl;
+#endif
+#ifdef DBG
+			cout << "h at t=" << t << endl;
+			util_printVec(h, hiddenDims);
+#endif
 		}
 	
 		// Classify
 		mulMatVec((ll*)qFC_Weight_l, h, numClasses, hiddenDims, out_numClasses);
 		addVecs(out_numClasses, (ll*)qFC_Bias_l, numClasses, out_numClasses);
-	
-		//printf("Classification output:\n");
-		//util_printVec(out_numClasses, numClasses);
-
+#ifdef DBG
+		cout << "Classification output:" << endl;
+		util_printVec(out_numClasses, numClasses);
+#endif
+#ifndef MOTE
 		//Print decision to csv file
 		string outstr;
 		for(int c = 0; c < numClasses -1 ; c++)
 			outstr += strBuild(out_numClasses[c], ',');
 		outstr += strBuild(out_numClasses[numClasses -1], '\n');
-		//cout << outstr;
 		outfile << outstr;
+#endif
 	}
+#ifndef MOTE
 	outfile.close();
+#endif
 }
