4942ae5c7601d635639242827fff012e9a5ab023
Dhrubojyoti Roy <royd@cse.ohio-state.edu>
2020-05-25 01:28:34 -0400

    Slightly modifying the test job

--- slurm_hpc/test_single_job.sh
+++ slurm_hpc/test_single_job.sh
@@ -5,4 +5,4 @@ module purge
 module load cudnn/9.0v7.3.0.29
 
 outname=test_out.out
-python3 ../tf/examples/EMI-RNN/step2_2tier_no_pretrain_joint_tvt.py -O 3 -gN sigmoid -uN tanh -bs 64 -H 64 -Dat /scratch/sk7898/buildsys_paper_data/bb_3class_winlen_256_winindex_all/3class_48_16 -rnd 5 -it 10 -ep 50 -ots 256 -k 10 -pretrain -out $outname
+python3 ../tf/examples/EMI-RNN/step2_2tier_joint_tvt.py -O 3 -gN sigmoid -uN tanh -bs 64 -H 16 -Dat /scratch/sk7898/buildsys_paper_data/bb_3class_winlen_256_winindex_all/3class_48_16 -rnd 5 -it 10 -ep 50 -ots 256 -k 10 -out $outname
