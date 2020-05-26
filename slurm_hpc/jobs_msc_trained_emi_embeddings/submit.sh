5c572ef1461e975c07b8a3fcb8a47ae3ec090706
Dhrubojyoti Roy <royd@cse.osu.edu>
2020-05-26 10:20:17 -0400

    Interval between jobs: 10 seconds

--- slurm_hpc/jobs_msc_trained_emi_embeddings/submit.sh
+++ slurm_hpc/jobs_msc_trained_emi_embeddings/submit.sh
@@ -1,6 +1,5 @@
 sbatch -t 1-0 --export=filename=msc_trained_emi_embeddings_H=64_winlen=256.sh batch_job.sbatch
 sleep 10
 sbatch -t 1-0 --export=filename=msc_trained_emi_embeddings_H=64_winlen=384.sh batch_job.sbatch
-sleep 1
+sleep 10
 sbatch -t 1-0 --export=filename=msc_trained_emi_embeddings_H=64_winlen=512.sh batch_job.sbatch
-sleep 1
